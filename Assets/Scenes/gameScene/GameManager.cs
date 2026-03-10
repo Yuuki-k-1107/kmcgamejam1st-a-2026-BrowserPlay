29cc4b2831adcddf4a81f99914f659e3427c759a
biuntri <biuntri.213@gmail.com>
2026-03-10 17:16:27 +0900

    ゲームマネージャを実装

---
+++ Assets/Scenes/gameScene/GameManager.cs
@@ -0,0 +1,30 @@
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+	ClockCon ClockCon;
+
+	#region アラーム
+	void GameStart()
+	{
+		UniTask.Run(ClockCon.AlarmStart);
+		//QTEも開始
+	}
+	#endregion
+
+	#region QTE関連
+	/// <summary>
+	/// アラームを止めるための仮
+	/// </summary>
+	public void AlarmStop()
+	{
+		ClockCon.AlarmStop();
+	}
+
+	public void QTEEnded(int combo)
+	{
+		//リザルト表示
+	}
+	#endregion
+}
