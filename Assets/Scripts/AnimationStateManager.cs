7b9f87677b3f1568dfd16ec84175f841a6be84c6
biuntri <biuntri.213@gmail.com>
2026-03-15 00:24:32 +0900

    ゲーム状態の変化を購読して、アニメーションオブジェクトのアクティベーションを切り替えるコードを追加

--- Assets/Scripts/AnimationStateManager.cs
+++ Assets/Scripts/AnimationStateManager.cs
@@ -12,6 +12,18 @@ public class AnimationStateManager : MonoBehaviour
 	private readonly ReactiveProperty<GameState> _State;
 	public ReadOnlyReactiveProperty<GameState> State => _State;
 
+	AnimationStateManager()
+	{
+		//アニメーションオブジェクトのアクティベーション
+		State.Subscribe(_ =>
+		{
+			outFromBedAnimObj.SetActive(State.CurrentValue == GameState.AlarmStoped);
+			intoBedAnimObj.SetActive(State.CurrentValue == GameState.Final);
+			BattlingAnimObj.SetActive(State.CurrentValue == GameState.Playing);
+			BattlingBedAnimObj.SetActive(State.CurrentValue == GameState.Playing);
+		});
+	}
+
 	public void Reset()
 	{
 		_State.Value = GameState.InBed;
